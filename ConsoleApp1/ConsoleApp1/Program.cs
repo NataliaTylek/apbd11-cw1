0742aa39be90b2592852f61ad1c82a2182704f29
NataliaTylek <s28827@pjwstk.edu.pl>
2025-03-15 22:07:17 +0100

    zmiana 1

--- ConsoleApp1/ConsoleApp1/Program.cs
+++ ConsoleApp1/ConsoleApp1/Program.cs
@@ -1,5 +1,26 @@
-﻿// See https://aka.ms/new-console-template for more information
-Console.WriteLine("Hello, World!");
-Console.WriteLine("Hello, World!");
-Console.WriteLine("Hello, World!");
-Console.WriteLine("Hello, World!");
+﻿using System;
+
+class Program
+{
+    static void Main()
+    {
+        int[] numbers = { 1, 2, 3, 4, 5 };
+        double average = CalculateAverage(numbers);
+        Console.WriteLine($"Średnia: {average}");
+    }
+
+    static double CalculateAverage(int[] numbers)
+    {
+        if (numbers == null || numbers.Length == 0)
+        {
+            throw new ArgumentException("Tablica nie może być pusta ani null.");
+        }
+
+        double sum = 0;
+        foreach (int num in numbers)
+        {
+            sum += num;
+        }
+        return sum / numbers.Length;
+    }
+}
